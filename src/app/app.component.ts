74b7cd58c5ad0edaef0e151960562714a303d3c1
Leonardo Mendes <leomendesrch2005@gmail.com>
2023-12-20 13:34:24 -0300

    introduced event-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'AngProject';
+
+  valorAtual: string = '';
+  valorSalvo: string = "";
+
+  isMouseOver: boolean = false;
+
+  botaoClicado(){
+    alert("Botão Clicado!");
+  }
+  
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (evento.target as HTMLInputElement).value
+  }
+
+  SalvarValor(elemento: HTMLInputElement | null){
+    this.valorSalvo = (elemento!).value
+  }
+
+  onMouseOverOut(elemento: HTMLSpanElement){
+    this.isMouseOver = !this.isMouseOver
+    console.log(elemento)
+  }
+
+
 }
